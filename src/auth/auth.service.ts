f9c016400ba5ebe518d259bf1be525aedde4a196
Matheusafonsouza <matheusafonsouza@gmail.com>
2024-12-07 15:58:49 -0300

    feat: ci workflow
    Co-Authored-By: Joaovitor045 <joaovitoralvestf@gmail.com>
    Co-Authored-By: Bruno Cruz <Bruno.g.rosa@hotmail.com.br>

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -5,6 +5,7 @@ import { Repository } from 'typeorm';
 import * as bcrypt from 'bcrypt';
 import { SignInDto } from './dtos/signIn.dto';
 import { AdminUser } from '../database/entities/admin-user.entity';
+import { SignInResponseDto } from './dtos/signInResponse.dto';
 
 @Injectable()
 export class AuthService {
@@ -14,16 +15,19 @@ export class AuthService {
     private jwtService: JwtService,
   ) {}
 
-  async signIn({ email, password }: SignInDto): Promise<any> {
+  async signIn({ email, password }: SignInDto): Promise<SignInResponseDto> {
     const user = await this.adminUsersRepository.findOneBy({ email });
-    if (!user)
-      throw new UnauthorizedException('Please check your login credentials');
-    if (!(await bcrypt.compare(password, user.password)))
-      throw new UnauthorizedException('Please check your login credentials');
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+      throw new UnauthorizedException('E-mail ou senha inválidos.');
+    }
+
+    const payload = { sub: user.id, email: user.email };
     return {
-      accessToken: await this.jwtService.signAsync({
-        sub: user.id,
-        email: user.email,
+      accessToken: await this.jwtService.signAsync(payload, {
+        expiresIn: '60m',
+      }),
+      refreshToken: await this.jwtService.signAsync(payload, {
+        expiresIn: '7d',
       }),
     };
   }
